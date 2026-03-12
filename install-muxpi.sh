b36a25cf28c17363698e74e697b041cf7c9e4f23
ewaldj <ewald@jeitler.cc>
2026-03-12 22:56:24 +0100

    Add install script for muxpi.sh
    This script checks the connection to GitHub.com and downloads muxpi.sh into a folder named muxpi.

---
+++ install-muxpi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# check connection 
+echo -e "\n### check connection to GitHub.com ### \n"
+if ! curl -s --head https://github.com | head -n 1 | grep "200" > /dev/null; then
+    echo -e "\n### Unable to connect to GitHub.com ### "
+    exit 1
+fi
+
+# install muxpi.sh to folder muxpi 
+echo -e "\n ### download and install muxpi.sh ###\n"
+mkdir -p muxpi 
+
+# download muxpi  
+cd muxpi  
+curl -O https://raw.githubusercontent.com/ewaldj/ejperf/main/muxpi.sh
+chmod +x muxpi.sh
+cd ..
+echo -e "\n### done - have a nice day - www.jeitler.guru ###\n" 
